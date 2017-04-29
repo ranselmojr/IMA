08094a25ff4f48851a8aa4d9d1a6c5f587b61b9c
Adam Swogger <adam.j.swogger@gmail.com>
2017-04-28 21:17:53 -0700

    version whatever

--- Prototype/MainPage.Designer.cs
+++ Prototype/MainPage.Designer.cs
@@ -43,10 +43,9 @@
             // picLogo
             // 
             this.picLogo.Image = global::Prototype.Properties.Resources.image;
-            this.picLogo.Location = new System.Drawing.Point(63, 61);
-            this.picLogo.Margin = new System.Windows.Forms.Padding(6);
+            this.picLogo.Location = new System.Drawing.Point(32, 32);
             this.picLogo.Name = "picLogo";
-            this.picLogo.Size = new System.Drawing.Size(200, 154);
+            this.picLogo.Size = new System.Drawing.Size(100, 80);
             this.picLogo.TabIndex = 1;
             this.picLogo.TabStop = false;
             // 
@@ -54,10 +53,9 @@
             // 
             this.lblUniversity.AutoSize = true;
             this.lblUniversity.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblUniversity.Location = new System.Drawing.Point(299, 185);
-            this.lblUniversity.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.lblUniversity.Location = new System.Drawing.Point(150, 96);
             this.lblUniversity.Name = "lblUniversity";
-            this.lblUniversity.Size = new System.Drawing.Size(239, 30);
+            this.lblUniversity.Size = new System.Drawing.Size(133, 16);
             this.lblUniversity.TabIndex = 2;
             this.lblUniversity.Text = "Unsung University";
             // 
@@ -67,9 +65,10 @@
             this.button1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button1.BackgroundImage")));
             this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
             this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.button1.Location = new System.Drawing.Point(149, 411);
+            this.button1.Location = new System.Drawing.Point(74, 214);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(362, 171);
+            this.button1.Size = new System.Drawing.Size(181, 89);
             this.button1.TabIndex = 3;
             this.button1.Text = "Grade     \r\nSheet     ";
             this.button1.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
@@ -82,9 +81,10 @@
             this.button2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button2.BackgroundImage")));
             this.button2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
             this.button2.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.button2.Location = new System.Drawing.Point(619, 411);
+            this.button2.Location = new System.Drawing.Point(310, 214);
+            this.button2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(362, 171);
+            this.button2.Size = new System.Drawing.Size(181, 89);
             this.button2.TabIndex = 4;
             this.button2.Text = "Attendance    \r\nSheet         ";
             this.button2.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
@@ -97,9 +97,10 @@
             this.button3.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button3.BackgroundImage")));
             this.button3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
             this.button3.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.button3.Location = new System.Drawing.Point(1069, 411);
+            this.button3.Location = new System.Drawing.Point(534, 214);
+            this.button3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(362, 171);
+            this.button3.Size = new System.Drawing.Size(181, 89);
             this.button3.TabIndex = 5;
             this.button3.Text = "Schedule    \r\nSheet        ";
             this.button3.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
@@ -112,9 +113,10 @@
             this.button4.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button4.BackgroundImage")));
             this.button4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
             this.button4.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.button4.Location = new System.Drawing.Point(149, 685);
+            this.button4.Location = new System.Drawing.Point(74, 356);
+            this.button4.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(362, 171);
+            this.button4.Size = new System.Drawing.Size(181, 89);
             this.button4.TabIndex = 6;
             this.button4.Text = "Lectures    ";
             this.button4.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
@@ -127,9 +129,10 @@
             this.button5.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button5.BackgroundImage")));
             this.button5.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
             this.button5.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.button5.Location = new System.Drawing.Point(619, 685);
+            this.button5.Location = new System.Drawing.Point(310, 356);
+            this.button5.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(362, 171);
+            this.button5.Size = new System.Drawing.Size(181, 89);
             this.button5.TabIndex = 7;
             this.button5.Text = "Student          \r\nDemographics    ";
             this.button5.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
@@ -142,9 +145,10 @@
             this.button6.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button6.BackgroundImage")));
             this.button6.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
             this.button6.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.button6.Location = new System.Drawing.Point(1069, 676);
+            this.button6.Location = new System.Drawing.Point(534, 352);
+            this.button6.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button6.Name = "button6";
-            this.button6.Size = new System.Drawing.Size(362, 171);
+            this.button6.Size = new System.Drawing.Size(181, 89);
             this.button6.TabIndex = 8;
             this.button6.Text = "Academic     \r\nHistory       ";
             this.button6.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
@@ -153,9 +157,9 @@
             // 
             // MainPage
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1557, 1057);
+            this.ClientSize = new System.Drawing.Size(784, 566);
             this.Controls.Add(this.button6);
             this.Controls.Add(this.button5);
             this.Controls.Add(this.button4);
@@ -164,11 +168,12 @@
             this.Controls.Add(this.button1);
             this.Controls.Add(this.lblUniversity);
             this.Controls.Add(this.picLogo);
-            this.MaximumSize = new System.Drawing.Size(1583, 1128);
-            this.MinimumSize = new System.Drawing.Size(1583, 1128);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.MaximumSize = new System.Drawing.Size(800, 605);
+            this.MinimumSize = new System.Drawing.Size(800, 605);
             this.Name = "MainPage";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "MainPage";
+            this.Text = "IMA User Home";
             this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.MainPage_FormClosed);
             ((System.ComponentModel.ISupportInitialize)(this.picLogo)).EndInit();
             this.ResumeLayout(false);
